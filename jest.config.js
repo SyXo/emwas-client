845248c1404decc05695beb74a5978daaaa7c76a
Fabien Leite <fabienleite@protonmail.com>
2019-04-12 20:35:27 +0200

    Switch tests to jest

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'vue',
+    'ts',
+  ],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  transformIgnorePatterns: [
+    '/node_modules/',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  snapshotSerializers: [
+    'jest-serializer-vue',
+  ],
+  testMatch: [
+    '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)',
+  ],
+  testURL: 'http://localhost/',
+};
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,13 +1,12 @@
-import { expect } from 'chai';
 import { shallowMount } from '@vue/test-utils';
 import Search from '@/components/Search.vue';
 
 describe('Search.vue', () => {
-  it('has one and only one search input', () => {
+  it('renders props.msg when passed', () => {
     const msg = 'new message';
     const wrapper = shallowMount(Search, {
       propsData: { msg },
     });
-    expect(wrapper.findAll('.search__input').length).to.eql(1);
+    expect(wrapper.findAll('.search__input').length).toEqual(1);
   });
 });
